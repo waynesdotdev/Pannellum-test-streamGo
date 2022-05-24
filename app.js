eba6fe22662521cbe659d29f025057c1ff8da37d
foxwayne1 <fox.wayne1@gmail.com>
2022-05-24 10:36:47 +0100

    done

---
+++ app.js
@@ -0,0 +1,49 @@
+pannellum.viewer('panorama', {
+  type: 'equirectangular',
+  panorama: './panarama.jpg',
+  // auto load image so no added step
+  autoLoad: true,
+  // simple title in corner
+  title: 'streamGo test',
+  author: 'Wayne Fox',
+
+  hotSpotDebug: true,
+  hotSpots: [
+    {
+      pitch: -11.77,
+      yaw: 0.05,
+      type: 'info',
+      text: 'Accreditation Info',
+    },
+    {
+      pitch: -10.99,
+      yaw: 60.24,
+      type: 'info',
+      text: 'Live Event',
+    },
+    {
+      pitch: -11.27,
+      yaw: 120.05,
+      type: 'info',
+      text: 'Agenda',
+    },
+    {
+      pitch: -10.99,
+      yaw: 179.65,
+      type: 'info',
+      text: 'Sponsor Hall',
+    },
+    {
+      pitch: -12.17,
+      yaw: -119.73,
+      type: 'info',
+      text: 'Conference Handout',
+    },
+    {
+      pitch: -11.25,
+      yaw: -59.73,
+      type: 'info',
+      text: 'The Speakers',
+    },
+  ],
+})
